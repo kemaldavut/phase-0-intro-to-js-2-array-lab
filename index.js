1a4433b825a8e8f2f23ee9fab4279aab3cc0c9aa
Kemal DAVUT <mustafakemaldavut@gmail.com>
2022-05-05 01:12:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+console.log("cats list = ",cats)
+
+function destructivelyAppendCat(name) {
+cats.push(name)
+}cle
+
+function destructivelyPrependCat(name) {
+cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+cats.shift()
+}
+
+function appendCat(name) {
+   return [...cats, name]
+}
+
+function prependCat(name) {
+    return [ name, ...cats]
+ }
+
+ function removeFirstCat() {
+return cats.slice(1, 3)
+ }
+
+ function removeLastCat() {
+return cats.slice(0, -1)
+}
